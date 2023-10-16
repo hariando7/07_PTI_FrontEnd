eb40d641a6d7dbfb9900cfc10e90e6cab391ff73
hariando7 <96804614+hariando7@users.noreply.github.com>
2023-10-16 14:22:47 +0700

    SettupAwal

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import NavbarPemesanan from './components/NavbarPemesanan'
+import Pemesanan from './pages/pemesanan/page'
+
+export default function Home() {
+  return (
+    <>
+    </>
+  )
+}
---
+++ src/app/pages/page.tsx
@@ -0,0 +1,10 @@
+import pemesanan from "./pemesanan/page"
+import Navbar from "../components/NavbarPemesanan"
+
+export default function Pemesanan() {
+    return (
+        <>
+            {/* <Navbar></Navbar> */}
+        </>
+    )
+}
